7e2f30c54c47099b8ba468416526d731afd330ef
peekmomo <1804131510@qq.com>
2026-04-30 22:49:08 +0800

    Add files via upload

--- array.js
+++ array.js
@@ -32,7 +32,6 @@ var merge = function(intervals) {
 };
 
 
-
 var rotate = function(nums, k) {
      let n = nums.length;
     k = k % n;
@@ -46,4 +45,21 @@ var rotate = function(nums, k) {
     reverse(nums, 0, n - 1);
     reverse(nums, 0, k - 1);
     reverse(nums, k, n - 1);
+};
+
+var productExceptSelf = function(nums) {
+     let n = nums.length;
+    let res = new Array(n).fill(1);
+    let left = 1;
+    for (let i = 0; i < n; i++) {
+        res[i] = left;
+        left *= nums[i];
+    }
+    let right = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        res[i] *= right;
+        right *= nums[i];
+    }
+
+    return res;
 };
